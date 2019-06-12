b61c0180687ae94886755f2a878f9d87952f9a88
Tiffany <n0isehazard@gmail.com>
2019-06-12 16:27:39 -0700

    Initial commit

---
+++ src/components/card.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="card" style="width: 18rem;">
+  <img src=""  alt="">
+  <div class="card-body">
+    <img v-bind:src="pokemon.sprites['front_default']" v-bind:alt="pokemon.name">
+    <!-- <img class="card-img-top" v-bind:src="pokemon.sprites.front_default" v-bind:alt="pokemon.name"> -->
+    <h5 class="text-capitalize card-title"> {{ pokemon.name }}</h5>
+    <p class="card-text mb-0" >Height: {{ pokemon.height }}</p>
+    <p class="card-text mt-0">Weight: {{ pokemon.weight }} </p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  data: function () {
+    return {
+    pokemon: null
+    }
+  },
+  props: {
+  msg: String
+  },
+  mounted: function() {
+
+    const axios = require('axios');
+    const vm = this;
+
+    axios({
+    method: 'get',
+    url: 'https://pokeapi.co/api/v2/pokemon/raichu',
+    responseType: 'stream'
+    })
+    .then(function (response) {
+      console.log(response.data);
+      vm.pokemon = response.data
+    });
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
